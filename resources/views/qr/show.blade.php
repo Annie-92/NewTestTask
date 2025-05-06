47a3f8e783e81a5c9ed8d902f28a3ea961351a18
Annie-92 <ani.keushguerian@gmail.com>
2025-05-06 19:07:43 +0400

    Update styling and switch to production-ready CSS reference

--- resources/views/qr/show.blade.php
+++ resources/views/qr/show.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Service Center</title>
-    @vite('resources/css/app.css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="{{ asset('build/assets/app-CVlZIbOT.css') }}" rel="stylesheet">
+
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body class="bg-gray-900 text-white min-h-screen flex flex-col">
 
